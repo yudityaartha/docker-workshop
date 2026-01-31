c892c42626246443cf4f7f2a8cb744d612bbf015
Yuditya Artha <yuditya_artha@outlook.com>
2026-01-31 13:53:12 +0000

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,7 +2,7 @@ import sys
 print("arguments", sys.argv)
 
 month = int(sys.argv[1])
-print(f"Running pipeline for month {month}")
+
 
 import pandas as pd
 df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
@@ -10,3 +10,5 @@ df['month'] = month
 print(df.head())
 
 df.to_parquet(f"output_month_{month}.parquet")
+
+print(f"Running pipeline for month {month}")
